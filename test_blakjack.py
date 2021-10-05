1c0b1436c2ec6c6d1aee46221aeac7a4ef3107a8
Ivanse90 <anfesega90@hotmail.com>
2021-10-04 20:51:42 -0500

    nuevo

---
+++ test_blakjack.py
@@ -0,0 +1,23 @@
+import pytest
+from BlackJack import Card #, Deck, Hand, dealer_wins, hit, hit_or_stand, show_some
+# # from test2 import show_all, player_busts, player_wins, dealer_busts, dealer_wins
+
+
+# @pytest.mark.parametrize(
+#     "input_a, input_b, expected",
+#     [
+#         ("Dos","Corazones", "Dos de Corazones"),
+#         ("Tres","Corazones", "Tres de Corazones"),
+#     ]
+
+# )
+# def test_card1(input_a,input_b,expected):
+#     assert Card(input_a,input_b) == expected
+
+
+# def test_card():
+#     assert Card("Dos","Corazones") == "Dos de Corazones"
+
+prueba = Card("Dos","Corazones")
+
+
